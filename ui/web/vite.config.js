2337e44c575d66fd2be94f400970bde1840c9aab
Mitch Avis <mitchavis@gmail.com>
2026-07-12 14:14:23 -0600

    Update Vite server port from 5173 to 5280 in vite.config.js

--- ui/web/vite.config.js
+++ ui/web/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        port: 5173,
+        port: 5280,
         proxy: {
             '/api': 'http://127.0.0.1:8000',
         },
